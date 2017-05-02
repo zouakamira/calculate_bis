09354fb8f71496fd0186a4a0ce68c50723cd3b12
tatsu <tatsu@hotmal.fr>
2017-05-02 14:30:03 +0200

    Non ! t'as faux!

--- calculate_bis/src/calculate_bis/Calculate_bis.java
+++ calculate_bis/src/calculate_bis/Calculate_bis.java
@@ -2,10 +2,10 @@ package calculate_bis;
 
 /**
 *Crée en travail groupé via GitHub et répartit par :
- *  ,
+ *  Amira
  *  Jacques,
- *  
- * 
+ *  Christophe
+ *  Pascal
  */
 import java.awt.BorderLayout;
 import java.awt.Color;
